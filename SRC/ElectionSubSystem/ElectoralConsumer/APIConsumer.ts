4ac7183e50b2771989732d104b2af2de26fdf23e
Joaquin Meerhoff <jm247096@fi365.ort.edu.uy>
2022-06-01 20:27:18 -0300

    Fixed election Generation

--- SRC/ElectionSubSystem/ElectoralConsumer/APIConsumer.ts
+++ SRC/ElectionSubSystem/ElectoralConsumer/APIConsumer.ts
@@ -1,21 +1,57 @@
 import Axios, { AxiosInstance } from "axios";
 import config from "config";
 import { IConsumer } from "./IConsumer";
-import { ElectionDTO } from "../../Common/Domain";
+import { ElectionDTO, VoterDTO } from "../../Common/Domain";
 import { HTTPRequestError } from "./../Errors/HTTPRequestError";
 import { Parameter } from "./Parameter";
 
 export class APIConsumer implements IConsumer {
   axios: AxiosInstance;
-
+  embedVoters: string;
+  
   constructor(parameters: Parameter[]) {
     parameters;
     this.axios = Axios.create({ baseURL: config.get("API.route") });
+    this.embedVoters = "?_embed=voters";
+  }
+  getVoterPaginated(
+    electionId: number,
+    page: number,
+    pageSize: number
+  ): Promise<VoterDTO[]> {
+    let endpoint: string = "/voters";
+    let specifyElection: string = "?electionId=" + electionId;
+    let specifyPage : string = "&_page=" + page;
+    let specifyLimit : string ="&_limit=" + pageSize;
+    let errorMessage: string =
+      "Error getting voters for election " + electionId;
+    electionId;
+
+    let finalEndpoint : string = endpoint + specifyElection + specifyPage + specifyLimit;
+    return this.axios
+      .get<VoterDTO[]>(finalEndpoint, {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      .then(function (response) {
+        let result: VoterDTO[] = response.data.map(
+          (election) => new VoterDTO(election)
+        );
+        return result;
+      })
+      .catch(function (error) {
+        throw new HTTPRequestError(errorMessage + " " + error.message);
+      });
   }
 
-  getElections(): Promise<ElectionDTO[]> {
-    let endpoint = "/elections";
+  getElections(includeVoters: boolean): Promise<ElectionDTO[]> {
+    let endpoint: string = "/elections";
+
     let errorMessage: string = "Error getting election";
+
+    if (includeVoters) endpoint += this.embedVoters;
+
     return this.axios
       .get<ElectionDTO[]>(endpoint, {
         headers: {
@@ -33,9 +69,12 @@ export class APIConsumer implements IConsumer {
       });
   }
 
-  getElection(id: number): Promise<ElectionDTO> {
+  getElection(id: number, includeVoters: boolean): Promise<ElectionDTO> {
     let endpoint: string = "/elections/" + id;
     let errorMessage: string = "Error getting election";
+
+    if (includeVoters) endpoint += this.embedVoters;
+
     return this.axios
       .get<ElectionDTO>(endpoint, {
         headers: {
--- Utilities/APIAutoridadElectoral/VoterGenerator/ElectionGenerator.js
+++ Utilities/APIAutoridadElectoral/VoterGenerator/ElectionGenerator.js
@@ -1,95 +1,117 @@
-const { faker } = require('@faker-js/faker');
-const CircuitGenerator = require('./CircuitGenerator');
-const PartyGenerator = require('./PartyGenerator');
-const CandidateGenerator = require('./CandidateGenerator');
-const VoterGenerator = require('./VoterGenerator');
+const { faker } = require("@faker-js/faker");
+const CircuitGenerator = require("./CircuitGenerator");
+const PartyGenerator = require("./PartyGenerator");
+const CandidateGenerator = require("./CandidateGenerator");
+const VoterGenerator = require("./VoterGenerator");
 
 class ElectionGenerator {
   constructor(circuitNumber, partyNumber, candidateNumber, voterNumber) {
     this.elections = [];
+    this.voters = [];
     this.circuitNumber = circuitNumber;
     this.partyNumber = partyNumber;
     this.candidateNumber = candidateNumber;
     this.voterNumber = voterNumber;
-    this.voters=[];
-    this.votersElection = [];
+    // this.votersElection = [];
     //this.generateAuxData();
   }
 
-    generateAuxData(electionId){
-        this.circuitGenerator = new CircuitGenerator();
-        this.partyGenerator = new PartyGenerator();
-        this.circuits = this.circuitGenerator.generateCircuits(this.circuitNumber);
-        this.parties = this.partyGenerator.generateParties(this.partyNumber);
+  generateAuxData(electionId) {
+    this.circuitGenerator = new CircuitGenerator();
+    this.partyGenerator = new PartyGenerator();
+    this.circuits = this.circuitGenerator.generateCircuits(this.circuitNumber);
+    this.parties = this.partyGenerator.generateParties(this.partyNumber);
 
-        this.candidateGenerator = new CandidateGenerator(this.parties);
-        this.candidates = this.candidateGenerator.generateCandidates(this.candidateNumber);
+    this.candidateGenerator = new CandidateGenerator(this.parties);
+    this.candidates = this.candidateGenerator.generateCandidates(
+      this.candidateNumber
+    );
 
-        this.voterGenerator = new VoterGenerator(this.circuits);
-        this.votersElection = this.voterGenerator.generateVoters(this.voterNumber, electionId);
+    this.voterGenerator = new VoterGenerator(this.circuits);
+    // this.votersElection = this.voterGenerator.generateVoters(this.voterNumber, electionId);
+  }
+
+  generateElection() {
+    let id = faker.unique(faker.datatype.number);
+    let name = "Election " + faker.name.jobArea();
+    let description = faker.lorem.sentence();
+    let startDate = this.formatDate(faker.date.future().toString());
+    let endDate = this.formatDate(faker.date.future(1, startDate));
+    let mode = ["unique", "repeated"][Math.floor(Math.random() * 2)];
+    //let circuits = this.getRandomSubarray(this.circuits, this.circuitNumber/3);
+    //let parties = this.getRandomSubarray(this.parties, this.circuitNumber/3);
+    //let candidates = this.getRandomSubarray(this.candidates, this.circuitNumber/3);
+    //let voters =  this.getRandomSubarray(this.voters, this.circuitNumber/3);
+    this.generateAuxData(id);
+    let circuits = this.circuits;
+    let parties = this.parties;
+    let candidates = this.candidates;
+
+    // let voters = this.voters;
+
+    // let election = { id, name, description, startDate, endDate, mode, circuits, parties, candidates, voters };
+    let election = {
+      id,
+      name,
+      description,
+      startDate,
+      endDate,
+      mode,
+      circuits,
+      parties,
+      candidates,
+    };
+
+    return election;
+  }
+
+  generateElections(numberOfElections) {
+    for (let i = 0; i < numberOfElections; i++) {
+      let election = this.generateElection();
+      this.elections.push(election);
+      let voters = this.voterGenerator.generateVoters(
+        this.voterNumber,
+        election.id
+      );
+      voters.forEach(v =>{
+          this.voters.push(v);
+      })
     }
 
-    generateElection() {
-        let id = faker.unique(faker.datatype.number);
-        let name = "Election "+ faker.name.jobArea();
-        let description = faker.lorem.sentence();
-        let startDate = this.formatDate(faker.date.future().toString());
-        let endDate = this.formatDate(faker.date.future(1, startDate));
-        let mode = ["unique", "repeated"][Math.floor(Math.random()*2)];
-        //let circuits = this.getRandomSubarray(this.circuits, this.circuitNumber/3);
-        //let parties = this.getRandomSubarray(this.parties, this.circuitNumber/3);
-        //let candidates = this.getRandomSubarray(this.candidates, this.circuitNumber/3);
-        //let voters =  this.getRandomSubarray(this.voters, this.circuitNumber/3);
-        this.generateAuxData(id);
-        let circuits = this.circuits;
-        let parties = this.parties;
-        let candidates = this.candidates;
+    return [this.elections, this.voters];
+  }
 
-        // let voters = this.voters;
+  formatDate(date) {
+    var d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear(),
+      hours = d.getHours(),
+      minutes = d.getMinutes(),
+      seconds = d.getSeconds();
 
-        // let election = { id, name, description, startDate, endDate, mode, circuits, parties, candidates, voters };
-        let election = { id, name, description, startDate, endDate, mode, circuits, parties, candidates};
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
 
-        return election;
+    return (
+      [year, month, day].join("-") + " " + [hours, minutes, seconds].join(":")
+    );
+  }
+
+  getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0),
+      i = arr.length,
+      min = i - size,
+      temp,
+      index;
+    while (i-- > min) {
+      index = Math.floor((i + 1) * Math.random());
+      temp = shuffled[index];
+      shuffled[index] = shuffled[i];
+      shuffled[i] = temp;
     }
-
-    generateElections(numberOfElections) {
-        for (let i = 0; i < numberOfElections; i++) {
-          this.elections.push(this.generateElection());
-          this.voters.push(this.votersElection);
-        }
-        
-        return [this.elections, this.voters];
-    }
-
-    formatDate(date) {
-        var d = new Date(date),
-            month = '' + (d.getMonth() + 1),
-            day = '' + d.getDate(),
-            year = d.getFullYear(),
-            hours = d.getHours(),
-            minutes = d.getMinutes(),
-            seconds = d.getSeconds();
-
-
-        if (month.length < 2)
-            month = '0' + month;
-        if (day.length < 2)
-            day = '0' + day;
-
-        return [year, month, day].join('-') + " " + [hours, minutes, seconds].join(':');
-        }
-
-        getRandomSubarray(arr, size) {
-        var shuffled = arr.slice(0), i = arr.length, min = i - size, temp, index;
-        while (i-- > min) {
-            index = Math.floor((i + 1) * Math.random());
-            temp = shuffled[index];
-            shuffled[index] = shuffled[i];
-            shuffled[i] = temp;
-        }
-        return shuffled.slice(min);
-    }
+    return shuffled.slice(min);
+  }
 }
 
 module.exports = ElectionGenerator;
