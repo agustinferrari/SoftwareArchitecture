7ba9dc670d3096f0b802b71973bd9d5762f0339f
Graziano Pascale <pascaleg99@gmail.com>
2022-06-11 21:32:46 -0300

    Notification settings finished

--- SRC/ConfigSubSystem/ConfigAPI/Controllers/ConfigController.ts
+++ SRC/ConfigSubSystem/ConfigAPI/Controllers/ConfigController.ts
@@ -1,24 +1,37 @@
 import { Request, Response } from "express";
-import { INotificationSettings} from "../Models/NotificationSettings";
+import { INotificationSettings } from "../Models/NotificationSettings";
 import { NotificationSettingsRepository } from "../../DataAccess/Repositories/NotificationSettingsRepository";
-import * as jwt from "jsonwebtoken";
+import { ElectionNotFound } from "../Errors/ElectionNotFound";
 
 export class ConfigController {
-
   static updateNotificationSettings = async (req: Request, res: Response) => {
-
-    try{
-  const notificationConfig : INotificationSettings = req.body;
-  console.log(notificationConfig)
-  await NotificationSettingsRepository.getNotificationSettingsRepository().updateNotificationSettings(notificationConfig);
-  res.status(200).send("Notification settings updated: "+ notificationConfig.toString());
-  }catch(err){
-    res.status(400).send("Invalid request: Incorrect format. Format: {maxVotesPerVoter: number, maxVoteReportRequestsPerVoter: number, emailsSubscribed: string[]}");
-    return;
-  }
-
-}
-
-
+    try {
+      const settings: INotificationSettings = req.body;
+      const updated =
+        await NotificationSettingsRepository.getNotificationSettingsRepository().updateNotificationSettings(
+          settings
+        );
+      res
+        .status(200)
+        .send(
+          "Notification settings for election " +
+            settings.electionId +
+            " updated: " +
+            JSON.stringify(updated)
+        );
+    } catch (error: any) {
+      if (error instanceof ElectionNotFound) {
+        res.status(404).send(error.message);
+      } else {
+        res
+          .status(400)
+          .send(
+            "Invalid request: Incorrect format. Format: {maxVotesPerVoter: number, maxVoteReportRequestsPerVoter: number, emailsSubscribed: string[]}"
+          );
+      }
+      return;
+    }
+  };
 }
 export default ConfigController;
+// "Invalid request: Incorrect format. Format: {maxVotesPerVoter: number, maxVoteReportRequestsPerVoter: number, emailsSubscribed: string[]}"
--- SRC/ConfigSubSystem/DataAccess/Repositories/NotificationSettingsRepository.ts
+++ SRC/ConfigSubSystem/DataAccess/Repositories/NotificationSettingsRepository.ts
@@ -1,34 +1,31 @@
-import mongoose from "mongoose";
-import {model } from "mongoose";
-import {INotificationSettings, notificationSettingsSchema } from "../../ConfigAPI/Models/NotificationSettings";
-import {NotificationSettingsQuery} from "../Query/NotificationSettingsQuery";
-import {NotificationSettingsCommand} from "../Command/NotificationSettingsCommand";
-import {ElectionCache} from "../../../Common/Redis/ElectionCache";
-import config from "config";
-import { Election } from "../../../Common/Domain";
-
+import { INotificationSettings } from "../../ConfigAPI/Models/NotificationSettings";
+import { NotificationSettingsCommand } from "../Command/NotificationSettingsCommand";
+import { CacheQuery } from "../../../Common/Redis/CacheQuery";
+import { CacheCommand } from "../../../Common/Redis/CacheCommand";
+import { ElectionNotFound } from "../../ConfigAPI/Errors/ElectionNotFound";
 
 export class NotificationSettingsRepository {
+  static _instance: NotificationSettingsRepository;
 
-  static _instance : NotificationSettingsRepository;
-
-  static getNotificationSettingsRepository(): NotificationSettingsRepository{
-    if(!NotificationSettingsRepository._instance){
+  static getNotificationSettingsRepository(): NotificationSettingsRepository {
+    if (!NotificationSettingsRepository._instance) {
       NotificationSettingsRepository._instance = new NotificationSettingsRepository();
     }
-      return NotificationSettingsRepository._instance;
-    
+    return NotificationSettingsRepository._instance;
   }
 
-   async updateNotificationSettings(
+  async updateNotificationSettings(
     newSettings: INotificationSettings
-  ): Promise<void> {
-
-    // if(ElectionCache.existsElection(newSettings.electionId)){
-      NotificationSettingsCommand.findOneAndUpdate(newSettings);
-    // }else{
-    //   throw new Error("Election with id "+newSettings.electionId+ " not found");
-    // }
-   }
-
+  ): Promise<INotificationSettings> {
+    const cacheQuery = new CacheQuery();
+    const cacheCommand = new CacheCommand();
+    const exists = await cacheQuery.existsElection(newSettings.electionId);
+    if (exists) {
+      const updated = await NotificationSettingsCommand.findOneAndUpdate(newSettings);
+      await cacheCommand.addNotificationSettings(updated);
+      return updated;
+    } else {
+      throw new ElectionNotFound(newSettings.electionId);
+    }
+  }
 }
