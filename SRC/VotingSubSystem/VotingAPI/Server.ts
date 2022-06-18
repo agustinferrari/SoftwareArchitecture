f5b9f2259183c23fcd03c8c631597601abd46e55
Agustin Ferrari <agustin_ferrari@hotmail.com>
2022-06-17 22:15:19 -0300

    Adds notification when votes validation fails or on timeout

--- SRC/VotingSubSystem/VotingAPI/Server.ts
+++ SRC/VotingSubSystem/VotingAPI/Server.ts
@@ -5,7 +5,7 @@ const express = require("express");
 import config from "config";
 import { TimeoutError } from "./Error/TimeOutError";
 import { checkJWTAndRole } from "./Middlewares/checkJWTAndRole";
-import {LoggerFacade} from "../Logger/LoggerFacade"
+import { LoggerFacade } from "../Logger/LoggerFacade";
 import { RequestStatus } from "./Models/RequestStatus";
 import { scheduleJob, RecurrenceRule } from "node-schedule";
 import { RequestCountHelper } from "../RequestCountHelper";
@@ -13,9 +13,9 @@ import { RequestCountHelper } from "../RequestCountHelper";
 class Server {
   public app: Application;
   private service: VotingService;
-  private logger : LoggerFacade;
-  private reqCountHelper : RequestCountHelper;
-  
+  private logger: LoggerFacade;
+  private reqCountHelper: RequestCountHelper;
+
   constructor(votingService: VotingService) {
     this.app = express();
     this.service = votingService;
@@ -26,16 +26,14 @@ class Server {
     let rule = new RecurrenceRule();
     rule.second = 0;
     scheduleJob(rule, async () => {
-     console.log(this.reqCountHelper);
-    })
+      console.log(this.reqCountHelper);
+    });
   }
-
 
   public start(): void {
     this.app.listen(this.app.get("port"), () => {
       console.log("Server on port: " + this.app.get("port"));
     });
-
   }
 
   private config(): void {
@@ -44,29 +42,46 @@ class Server {
   }
 
   private routes(): void {
-    this.app.post("/votes", /*checkJWTAndRole(["Voter"]),*/ async (req: Request, res: Response, next : NextFunction) => {
-      req.setTimeout(0);
-      this.reqCountHelper.expressCount++;
-      res.status(200).send("Voto procesado");
-      next();
-    }, async (req: Request)=>{
-      req.setTimeout(0);
-      this.reqCountHelper.nextFunction++;
-      try {
-        let converted = req.body;
-        let requestStatus = new RequestStatus(converted.ci, new Date());
-
-        let voteIntent: VoteIntentEncrypted = converted as VoteIntentEncrypted;
-        await this.service.handleVote(voteIntent);
-      } catch (e: any) {
-        this.reqCountHelper.errorCount++;
-        this.reqCountHelper.errorType.push(e.message)
-        if (e instanceof TimeoutError) {
+    this.app.post(
+      "/votes",
+      /*checkJWTAndRole(["Voter"]),*/ async (req: Request, res: Response, next: NextFunction) => {
+        let startResponse = new Date();
+        let ci = req.body.ci;
+        req.setTimeout(0);
+        this.reqCountHelper.expressCount++;
+        let endResponse = new Date();
+        let responseTime = endResponse.valueOf() - startResponse.valueOf();
+        let timeout: number = config.get("VOTING_API.timeout");
+        if (responseTime > timeout) {
+          let message = `Timeout on vote for ci ${ci}`;
+          this.logger.logServerError(message, "/votes");
+          res.status(500).send(message);
         } else {
-          this.logger.logBadRequest(`Voting issue for ci ${req.body.voterCI}: ${e.message}`,req.originalUrl)
+          res.status(200).send("Voto procesado");
+          next();
+        }
+      },
+      async (req: Request) => {
+        req.setTimeout(0);
+        this.reqCountHelper.nextFunction++;
+        try {
+          let converted = req.body;
+
+          let voteIntent: VoteIntentEncrypted = converted as VoteIntentEncrypted;
+          await this.service.handleVote(voteIntent);
+        } catch (e: any) {
+          this.reqCountHelper.errorCount++;
+          this.reqCountHelper.errorType.push(e.message);
+          if (e instanceof TimeoutError) {
+          } else {
+            this.logger.logBadRequest(
+              `Voting issue for ci ${req.body.voterCI}: ${e.message}`,
+              req.originalUrl
+            );
+          }
         }
       }
-    });
+    );
   }
 }
 
--- SRC/VotingSubSystem/VotingAPI/VotingService.ts
+++ SRC/VotingSubSystem/VotingAPI/VotingService.ts
@@ -1,26 +1,23 @@
 import { VoteIntentEncrypted } from "./Models/VoteIntentEncrypted";
-import { VoteIntent } from "./Models/VoteIntent";
 import { VoteEncryption } from "../VoteEncryption";
 import { Vote, ElectionInfo, Voter, VoteProof } from "../../Common/Domain";
 import { VoteCommand } from "../DataAccess/Command/VoteCommand";
 import { Query } from "../DataAccess/Query/Query";
 import { ValidatorManager } from "../Validators/ValidatorManager";
 import { INotificationSender } from "../../Common/NotificationSender";
-import { TimeoutError } from "./Error/TimeOutError";
-import { RequestStatus } from "./Models/RequestStatus";
 import { RequestCountHelper } from "../RequestCountHelper";
 import { LoggerFacade } from "../Logger/LoggerFacade";
-import config from "config";
 export class VotingService {
   voteCommand: VoteCommand;
   voteQuery: Query;
   validatorManager: ValidatorManager;
-  voteProofSender : INotificationSender;
+  notificationSender: INotificationSender;
   loggerFacade: LoggerFacade;
+
   constructor(voteCommand: VoteCommand, voteQuery: Query, voteProofSender: INotificationSender) {
     this.voteCommand = voteCommand;
     this.voteQuery = voteQuery;
-    this.voteProofSender = voteProofSender;
+    this.notificationSender = voteProofSender;
     this.validatorManager = new ValidatorManager(voteQuery);
     this.loggerFacade = new LoggerFacade();
   }
@@ -28,29 +25,24 @@ export class VotingService {
   async handleVote(voteIntentEncrypted: VoteIntentEncrypted): Promise<void> {
     let reqCountHelper = RequestCountHelper.getInstance();
 
-
-    let startTimestamp = new Date();
+    //TODO ver que era esto
+    let startTimestamp = new Date(); //req.noseque
     reqCountHelper.beforeGetVoter++;
-    
-    let voter : Voter = await this.voteQuery.getVoter(voteIntentEncrypted.voterCI);
-    
+
+    let voter: Voter = await this.voteQuery.getVoter(voteIntentEncrypted.voterCI);
+
     reqCountHelper.beforeEncryptionCount++;
     let vote = await VoteEncryption.decryptVote(voteIntentEncrypted, voter);
-    
+
     reqCountHelper.beforeValidationCount++;
     await this.validatorManager.createPipeline(vote, "vote");
-    await this.validatorManager.validate();
-
-    let endTimestamp = new Date();
-    vote.endTimestamp = endTimestamp;
-
-    let responseTime = endTimestamp.valueOf() - vote.startTimestamp.valueOf();
-
-    let timeout :number= config.get("VOTING_API.timeout");
-    if (responseTime > timeout) {
-      let message = `Timeout on vote for ci ${vote.voterCI} and election ${vote.electionId}`;
-      this.loggerFacade.logServerError(message, "/votes");
-      throw new TimeoutError();
+    try {
+      await this.validatorManager.validate();
+    } catch (error: any) {
+      this.notificationSender.sendNotification(`Could not validate vote:/n${error.message}`, [
+        voter.phone,
+      ]);
+      return;
     }
 
     reqCountHelper.afterValidationCount++;
@@ -58,7 +50,7 @@ export class VotingService {
     return;
   }
 
-  private async afterValidation(vote: Vote, voter: Voter, startTimestamp :Date){
+  private async afterValidation(vote: Vote, voter: Voter, startTimestamp: Date) {
     let election: ElectionInfo = await this.voteQuery.getElection(vote.electionId);
     vote.randomizeAndSetId();
     let reqCountHelper = RequestCountHelper.getInstance();
@@ -69,17 +61,17 @@ export class VotingService {
     this.sendVoteProof(vote, voter, election);
   }
 
-  private async addVote(vote: Vote, election :ElectionInfo, startTimestamp: Date) {
+  private async addVote(vote: Vote, election: ElectionInfo, startTimestamp: Date) {
     let responseTime = vote.endTimestamp.valueOf() - startTimestamp.valueOf();
     vote.responseTime = responseTime;
 
     return this.voteCommand.addVote(vote, election.mode);
   }
 
-  private sendVoteProof(vote: Vote, voter: Voter, election : ElectionInfo){
-    let voteProof : VoteProof = new VoteProof(vote,voter,election);
+  private sendVoteProof(vote: Vote, voter: Voter, election: ElectionInfo) {
+    let voteProof: VoteProof = new VoteProof(vote, voter, election);
     let message = voteProof.ToString();
-    let phoneNumbers : string[]= [voter.phone];
-    this.voteProofSender.sendNotification(message,phoneNumbers);
+    let phoneNumbers: string[] = [voter.phone];
+    this.notificationSender.sendNotification(message, phoneNumbers);
   }
 }
